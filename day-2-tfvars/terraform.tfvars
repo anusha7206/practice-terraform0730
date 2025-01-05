5fcb25924d32c351321eec507bf27735819c2816
asamala <asamala514@gmail.com>
2025-01-05 20:35:55 +0530

    day-2-tfvars

---
+++ day-2-tfvars/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id   = "ami-0fd05997b4dff7aac"
+type     = "t2.micro"
+key_name = "aws_key"
---
+++ day-2-tfvars/variables.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+  description = "inserting ami value to main.tf"
+  type        = string
+  default     = ""
+
+}
+
+variable "type" {
+  description = "inserting type value to main.tf"
+  type        = string
+  default     = ""
+
+}
+variable "key_name" {
+  description = "inserting key value to main.tf"
+  type        = string
+  default     = ""
+
+}
